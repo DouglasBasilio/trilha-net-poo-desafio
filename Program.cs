2ec3b67e61e066b35e047f701fce0d374c532f6c
Douglas Basilio <basilio.douglas@yahoo.com.br>
2023-10-17 15:57:31 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Realizar os testes com as classes Nokia e Iphone
+
+            var nokia = new Nokia("123456789");
+            var iphone = new Iphone("987654321");
+
+            nokia.Ligar();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            iphone.Ligar();
+            iphone.InstalarAplicativo("Facebook");
+        }
+    }
+}
